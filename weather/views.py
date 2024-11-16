05e20ae7cae1414eb905f9f740423a9d75b0730d
MohammadEslahi <9516email@gmail.com>
2024-11-16 23:30:15 +0330

    results visually improved

--- weather/views.py
+++ weather/views.py
@@ -11,10 +11,17 @@ def home(request):
 
     if request.method =='POST':
         city = request.POST.get('city')
-        api_key = ''
+        api_key = '05f3869f87eb5393b6a5b843900757d6'
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
         response = requests.get(url)
         data = response.json()
-        return render(request, 'home.html', {'data':data})
+
+        context = {
+            'city' : request.POST.get('city'),
+            'weather' : data['weather'][0]['description'],
+            'temperature': data['main']['temp'] - 273.15,
+        }
+
+        return render(request, 'home.html', context)
     else:
         return render(request, 'home.html')
