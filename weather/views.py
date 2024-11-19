3f9d004b483af0c0c7ce3f796e5f3e2a205309fb
MohammadEslahi <9516email@gmail.com>
2024-11-19 10:05:29 +0330

    Temperature unit selector added

--- weather/views.py
+++ weather/views.py
@@ -13,17 +13,29 @@ def home(request):
     error = None
 
     if request.method =='POST':
+        # second parameter defines the default option
+        unit = request.POST.get('unit', 'C')
         city = request.POST.get('city')
         api_key = os.getenv('WeatherAPI-key')
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
         response = requests.get(url)
         data = response.json()
+        print(unit)
+        
         if data.get("cod") == 200:
+            if unit == 'C':
+                temperature = round(data['main']['temp'] - 273.15) # rounds it to the nearest whole number
+                unit_symbol = 'C'
+            else:
+                temperature = round((data['main']['temp'] - 273.15)*1.8 +32) # converts to F
+                unit_symbol = 'F'
             context = {
                 'data': data,
                 'city' : request.POST.get('city'),
                 'weather' : data['weather'][0]['description'],
-                'temperature': round(data['main']['temp'] - 273.15), #rounds it to the nearest whole number
+                'temperature': temperature,
+                'unit': unit,
+                'unit_symbol' : unit_symbol,
             }
 
             return render(request, 'home.html', context)
