d405320e454a9d60baec9827f9e537a716b268e7
MohammadEslahi <9516email@gmail.com>
2024-11-21 15:18:33 +0330

    CustomUser created

--- CONFIG/urls.py
+++ CONFIG/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('weather.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
