ef5b9b47989962561fa2fcfdfdc501c02476d94c
MohammadEslahi <9516email@gmail.com>
2024-11-26 15:25:25 +0330

    Edit account added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import *
-
+from . import views
 
 urlpatterns = [
     path('registration/', CustomUserCreationView.as_view(), name='registration'),
+    path("edituser/<int:id>/", views.editAccount, name='edit_account'),
 ]
