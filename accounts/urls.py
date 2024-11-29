dacad71f0f177700799dce86f6c224b96bbe40c8
MohammadEslahi <9516email@gmail.com>
2024-11-29 21:22:39 +0330

    displaying error messages

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('registration/', CustomUserCreationView.as_view(), name='registration'),
     path("edituser/<int:id>/", views.editAccount, name='edit_account'),
-    path('', views.profile, name='profile'),
 ]
