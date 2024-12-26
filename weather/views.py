58e6b65f4dbdc78414670d97d495b41fe66b22ae
MohammadEslahi <9516email@gmail.com>
2024-12-26 20:18:14 +0000

    precise API error handling

--- weather/views.py
+++ weather/views.py
@@ -1,29 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import requests
-# instead of hardcoding API-key
+from django.contrib import messages
 import os
 from dotenv import load_dotenv
 from accounts.models import *
 
 load_dotenv()
 
+
+
 def home(request):
     city = ''
     temperature = None
     weather = None
     error = None
+    is_fav = False
 
     if request.user.is_authenticated:
+        # Checking if entered city is Fav-ed before
         if request.user.fav_cities.filter(name=request.POST.get('city')).exists():
             is_fav = True
         else:
             is_fav = False
 
     if request.method =='POST':
-        # adding favorite cities (checks name of html button)
+        # adding favorite cities
         if request.user.is_authenticated:
             if 'Add city to fav' in request.POST:
-                searched_city = request.POST.get('city') # city that user has searched.
+                searched_city = request.POST.get('city')
                 City.objects.get_or_create(name=searched_city)
                 request.user.fav_cities.add(City.objects.get(name=searched_city))
                 is_fav = True
@@ -38,45 +42,41 @@ def home(request):
         unit = request.POST.get('unit', 'C')
         city = request.POST.get('city')
         api_key = os.getenv('WeatherAPI-key')
+
+
+        if not api_key:
+            return render(request,'home.html', {'error':"API-key not provided, you may need to add one."})
+
+        if not city:
+            return render(request,'home.html',{'error':"We won't know where to look for, if you haven't written a city name :)"})
+
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
         response = requests.get(url)
         data = response.json()
         print(data)
-        if not api_key:
-            return render(request,'home.html', {'error':"API-key not provided, you may need to add one."})
         
-        elif not city:
-            return render(request,'home.html',{'error':"We won't know where to look for, if you haven't written a city name :)"})
-        
-        else:
-            if data.get("cod") == 200:
-                if unit == 'C':
-                    temperature = round(data['main']['temp'] - 273.15) # rounds it to the nearest whole number
-                    unit_symbol = '°C'
-                else:
-                    temperature = round((data['main']['temp'] - 273.15)*1.8 +32) # converts to F
-                    unit_symbol = 'F'
-                if request.user.is_authenticated:
-                    context = {
-                        'data': data,
-                        'city' : request.POST.get('city'),
-                        'weather' : data['weather'][0]['description'],
-                        'temperature': temperature,
-                        'unit': unit,
-                        'unit_symbol' : unit_symbol,
-                        'is_fav': is_fav,
-                    }
-                else:
-                      context = {
-                        'data': data,
-                        'city' : request.POST.get('city'),
-                        'weather' : data['weather'][0]['description'],
-                        'temperature': temperature,
-                        'unit': unit,
-                        'unit_symbol' : unit_symbol,
-                    }
-                return render(request, 'home.html', context)
+        if data.get("cod") == 200:
+            if unit == 'C':
+                temperature = round(data['main']['temp'] - 273.15) # rounds it to the nearest whole number
+                unit_symbol = '°C'
             else:
-                return render(request, 'home.html', {'error':"We looked every single corner, but couldn't find ", 'city':city or ''})
+                temperature = round((data['main']['temp'] - 273.15)*1.8 +32) # converts to F
+                unit_symbol = 'F'
+            context = {
+                'data': data,
+                'city' : request.POST.get('city'),
+                'weather' : data['weather'][0]['description'],
+                'temperature': temperature,
+                'unit': unit,
+                'unit_symbol' : unit_symbol,
+                'is_fav': is_fav,
+            }
+            return render(request, 'home.html', context)
+            
+        elif data.get('cod')== 401:
+                messages.error(request, 'API-key is not valid.')
+                return redirect('home')
+        else:
+            return render(request, 'home.html', {'error':"We looked every single corner, but couldn't find ", 'city':city or ''})
     else:
         return render(request, 'home.html', {'city':city or '','error':error})
